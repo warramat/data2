a7f159f66ea41d922046a8dbd5c7280be7ec3b45
faisorn6 <faisorn6@gmail.com>
2020-11-03 13:30:29 +0700

    intern

--- src/view/dashboard/Dashboard.js
+++ src/view/dashboard/Dashboard.js
@@ -1,117 +1,119 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
-import {Row, Col, Skeleton,Card } from 'antd';
-import {
-    GET_DATA_SPORTMAN,
-} from '../../service/api'
-// import Swal from "sweetalert2";
-// import { _isEmpty} from '../../tools/util'
-// import { Bar } from "react-chartjs-2";
+import { GET_DATA } from '../../service/api'
+import { Bar } from "react-chartjs-2";
+import moment from 'moment'
 
-const Dashboard = (props) => {
-    const [loading,setLoading] = useState(false)
-    const [label,setLabel] = useState([])
-    const [dataInfo,setDataInfo] = useState([])
+const Test = () => {
+  const [labelChartOne, setLabelChartOne] = useState([])
+  const [dataInfo, setDataInfo] = useState([])
 
-    useEffect(() => {
-        GetData()
-    }, [])
+  useEffect(() => {
+    GetData()
+  }, [])
 
-        const data = {
-          labels: [
-            "บ้านตนเอง(ไม่มีค่าใช้จ่าย)",
-            "โรงแรม",
-            "รีสอร์ท",
-            "อุทยาน",
-            "อื่นๆ",
-          ],
-          datasets: [
-            {
-              barThickness: 25,
-              data: [8000, 5000, 7000, 5000, 8000],
-              backgroundColor: "#13eecc",
-            },
-          ],
-        };
+  const GetData = async () => {
+    try {
+      const res = await GET_DATA()
+      console.log("RES>>>", res)
 
-        const option = {
-          legend: {
-            display: false,
-          },
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  min: 0,
-                  max: 10000,
-                  stepSize: 2000,
-                },
-                
-              },
-            ],
-          },
-        };
+      if (res.code === 200) {
+        setLabelChartOne(res.result[0].residence.label)
+        setDataInfo(res.result[0].residence.data)
+        // console.log("res>>>",res.result[0].residence.label)
+      } else {
+        alert("ERROR", res)
+      }
 
-    const GetData = async () => {
-        try {
-
-        } catch (error) {
-            console.log(error);
-        }
-
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    return (
-        <Skeleton loading={loading} active>
-            <div className="BG-forms">
-            {/* <Bar data={data} options={option} /> */}
-            <Row>NAVBAR</Row>
-            <Row>SUB NAVBAR</Row>
-            <Row>
-                <Col lg={4}>
-                    <Card>1</Card>
-                </Col>
-                <Col lg={4}>
-                    <Card>2</Card>
-                </Col>
-                <Col lg={4}>
-                    <Card>3</Card>
-                </Col>
-                <Col lg={4}>
-                    <Card>4</Card>
-                </Col>
-                <Col lg={4}>
-                    <Card>5</Card>
-                </Col>
-                <Col lg={4}>
-                    <Card>6</Card>
-                </Col>
-            </Row>
-            <Row>
-                <Col lg={8}>
-                    <Card>1</Card>
-                </Col>
-                <Col lg={8}>
-                    <Card>2</Card>
-                </Col>
-                <Col lg={8}>
-                    <Card>3</Card>
-                </Col>
-            </Row>
-            <Row>
-                <Col lg={8}>
-                    <Card>1</Card>
-                </Col>
-                <Col lg={8}>
-                    <Card>2</Card>
-                </Col>
-                <Col lg={8}>
-                    <Card>3</Card>
-                </Col>
-            </Row>
-            </div>
-        </Skeleton>
-    )
+  const data = {
+    labels: labelChartOne,
+    datasets: [
+      {
+        barThickness: 25,
+        data: dataInfo,
+        backgroundColor: "#13eecc",
+      },
+    ],
+  };
+
+  const option = {
+    legend: {
+      display: false,
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            max: 5,
+            stepSize: 2000,
+          },
+
+        },
+      ],
+    },
+  };
+
+  return (
+    <div className="BG-forms">
+      <Row>NAVBAR</Row>
+      <Row>SUB NAVBAR</Row>
+      <Row>
+        <Col lg={4}>
+          <Card>
+            {
+              labelChartOne.map((item, index) => {
+                return <div key={index}>{item}</div>
+              })
+            }</Card>
+        </Col>
+        <Col lg={4}>
+          <Card>2</Card>
+        </Col>
+        <Col lg={4}>
+          <Card>3</Card>
+        </Col>
+        <Col lg={4}>
+          <Card>4</Card>
+        </Col>
+        <Col lg={4}>
+          <Card>5</Card>
+        </Col>
+        <Col lg={4}>
+          <Card>6</Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={8}>
+          <Card>
+            <Bar data={data} options={option} />
+          </Card>
+        </Col>
+        <Col lg={8}>
+          <Card>2</Card>
+        </Col>
+        <Col lg={8}>
+          <Card>3</Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={8}>
+          <Card>1</Card>
+        </Col>
+        <Col lg={8}>
+          <Card>2</Card>
+        </Col>
+        <Col lg={8}>
+          <Card>3</Card>
+        </Col>
+      </Row>
+    </div>
+  )
 }
 
-export default Dashboard
+export default Test
