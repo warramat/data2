e28ed2291b4522e97120857357fbd9e07d7638a4
faisorn6 <faisorn6@gmail.com>
2020-11-03 12:14:52 +0700

    ChartIntern ISAT

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,7 @@
-import ExhibitorsTH from "./view/questionnaireformsTh/ExhibitorsTH"
-import SatisfactionTH from "./view/questionnaireformsTh/SatisfactionTH"
-import ExhibitorsEN from "./view/questionnaireformsEn/ExhibitorsEN"
-import SatisfactionEN from "./view/questionnaireformsEn/SatisfactionEN"
+
+import Test from "./view/Test"
 
 export const routes = [
-  { path: "/", exact: true, name: "ExhibitorsTH", component: ExhibitorsTH  },
-  { path: "/exhibitorsTh", name: "ExhibitorsTH", component: ExhibitorsTH, exact: true },
-  { path: "/satisfactionTh", name: "SatisfactionTH", component: SatisfactionTH, exact: true },
-  { path: "/exhibitorsEN", name: "ExhibitorsEN", component: ExhibitorsEN, exact: true },
-  { path: "/satisfactionEN", name: "SatisfactionEN", component: SatisfactionEN, exact: true },
+  { path: "/", exact: true, name: "Test", component: Test  },
 ];
 
