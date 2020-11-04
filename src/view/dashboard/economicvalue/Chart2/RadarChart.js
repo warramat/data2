54e66db97a255ed82bce9b8c1341c758c83e5414
warramat <warramat1999@gmail.com>
2020-11-04 19:30:29 +0700

    warra

--- src/view/dashboard/economicvalue/Chart2/RadarChart.js
+++ src/view/dashboard/economicvalue/Chart2/RadarChart.js
@@ -2,10 +2,6 @@ import React from 'react'
 import { Radar } from '@reactchartjs/react-chart.js'
 import {  Col, Row } from 'react-bootstrap';
 
-const RadarChart = ({ dataSource }) => {
-    console.log("dataSource>>", dataSource)
-
-
 const data = {
   labels: ['ผู้จัดการแข่งขัน ', 'นักกีฬาต่างชาติ', 'ผู้ติดตาม / ผู้ชม / ร้านอาหาร', 'อาสาสมัคร' , 'นักกีฬาไทย'],
   datasets: [
@@ -13,7 +9,7 @@ const data = {
       data: [4.9, 4.2, 4.0, 4.0, 4.1],
       borderColor: '#13EECC',
       backgroundColor: '#13EECC70',
-      borderWidth: 2,
+      borderWidth: 1,
       display: false,
       
     },
@@ -38,79 +34,22 @@ const options = {
 
 const RadarChart = () => (
  
-  <Row style={{ width: "480px", height: "300px" }}>
+  <Row style={{ width: "450px", height: "250px" }}>
     
     <Col >
-    <div >
-    <b style={{font: "normal normal bold 20px/35px Sukhumvit Set" , color: "#292766", marginLeft: 5 }}>ค่าตัวทวีคูณทางเศรษฐกิจ</b></div>
-    <b style={{font: "normal normal bold 15px/25px Sukhumvit Set" , color: "#292766", marginLeft: 5 }}>ตามประเภทผู้ประเมิน</b>
+    <div className='header'>
+    <b style={{font: "normal normal bold 15px/35px Sukhumvit Set" , color: "#292766", marginLeft: 20 }}>ค่าตัวทวีคูณทางเศรษฐกิจ</b></div>
+    <b style={{font: "normal normal bold 10px/25px Sukhumvit Set" , color: "#292766", marginLeft: 20 }}>ตามประเภทผู้ประเมิน</b>
    
-    <div style={{left: 20}}><Radar  data={data} options={options}  /></div>
+    <div><Radar  data={data} options={options}  /></div>
 
     </Col>
+
+
     </Row>
     
  
 )
-}
 
 export default RadarChart
-
-
-
-
-// import React from 'react'
-// import { Radar } from '@reactchartjs/react-chart.js'
-// import {  Col, Row } from 'react-bootstrap';
-
-// const data = {
-//   labels: ['ผู้จัดการแข่งขัน ', 'นักกีฬาต่างชาติ', 'ผู้ติดตาม / ผู้ชม / ร้านอาหาร', 'อาสาสมัคร' , 'นักกีฬาไทย'],
-//   datasets: [
-//     {
-//       data: [4.9, 4.2, 4.0, 4.0, 4.1],
-//       borderColor: '#13EECC',
-//       backgroundColor: '#13EECC70',
-//       borderWidth: 2,
-//       display: false,
-      
-//     },
-//   ],
-// }
-// const options = {
-
-//   legend: {
-//     display: false,
-//       borderRadius: 20  
-     
-   
-//  },
-//   scale: {
-//     ticks: { beginAtZero: true },
-    
-//   },
-  
-// }
-
-
-
-// const RadarChart = () => (
- 
-//   <Row style={{ width: "450px", height: "250px" }}>
-    
-//     <Col >
-//     <div className='header'>
-//     <b style={{font: "normal normal bold 20px/35px Sukhumvit Set" , color: "#292766", marginLeft: 5 }}>ค่าตัวทวีคูณทางเศรษฐกิจ</b></div>
-//     <b style={{font: "normal normal bold 15px/35px Sukhumvit Set" , color: "#292766", marginLeft: 5 }}>ตามประเภทผู้ประเมิน</b>
-   
-//     <div><Radar  data={data} options={options}  /></div>
-
-//     </Col>
-
-
-//     </Row>
-    
- 
-// )
-
-// export default RadarChart
 
