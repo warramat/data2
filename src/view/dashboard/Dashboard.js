b07f9d03f3b6abb889934227780b715b9382af62
Ridwan <ridwan72th@gmail.com>
2020-11-06 18:23:58 +0700

    update layout

--- src/view/dashboard/Dashboard.js
+++ src/view/dashboard/Dashboard.js
@@ -6,7 +6,7 @@ import Satisfaction from "./satisfaction/Satisfaction"
 import { Row, Col, Radio, } from 'antd'
 import Doiintanon from '../../assets/image/logo/doiintanon.svg'
 import iSat from '../../assets/image/logo/Sat.svg'
-import { Spin, Space } from 'antd';
+import { Spin } from 'antd';
 
 const Dashboard = () => {
   const [key, setKey] = useState("respon")
@@ -14,24 +14,24 @@ const Dashboard = () => {
 
   return (
     <div className="BG-forms font-kanit spinner">
-      {/* <Space size="middle"> */}
 
-
-      {/* <Space size="middle"> */}
-
-      <Row className="w-100 nav p-15 h-25 " align={"middle"}>
+      <Row className="w-100 nav p-15 h-25 " align={"middle"} justify={"center"}>
         <Col lg={7}>
-          <div className="pl-40 align-items-center">
+          <div className="pl-33 align-items-center">
             <img src={iSat} alt="iSat" width={"53%"} className="pr-2" />
             <span className="hr-horizon"></span><img src={Doiintanon} alt="doiintanonLogo" width={"20%"} />
           </div>
         </Col>
-        <Col lg={10} className="d-flex justify-content-center">
+        <Col lg={10} >
+
           <Radio.Group onChange={(e) => setKey(e.target.value)} value={key} >
-            <Radio.Button value="respon">ผู้ตอบแบบสอบถาม</Radio.Button>
-            <Radio.Button value="economic">มูลค่าทางเศรษฐกิจ</Radio.Button>
-            <Radio.Button value="satis">ระดับความพึงพอใจ</Radio.Button>
+            <Row justify={"center"}>
+              <Col><Radio.Button value="respon">ผู้ตอบแบบสอบถาม</Radio.Button> </Col >
+              <Col><Radio.Button value="economic">มูลค่าทางเศรษฐกิจ</Radio.Button></Col>
+              <Col><Radio.Button value="satis">ระดับความพึงพอใจ</Radio.Button></Col>
+            </Row>
           </Radio.Group>
+
         </Col>
         <Col lg={7}></Col>
       </Row>
