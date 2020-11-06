26d4165df05f0722a20181b91dbb154ca3ce4c4c
shopalret <salinda_fern@hotmail.com>
2020-11-06 16:15:17 +0700

    update

--- src/config/ApiServer.js
+++ src/config/ApiServer.js
@@ -1,4 +1,4 @@
-// export const apiURL = "http://192.168.1.121:7009";
-export const apiURL = "http://196.53.248.14";
+export const apiURL = "http://192.168.1.121:7009";
+//export const apiURL = "http://196.53.248.14";
 
 export const apiV1 = "/api/v1";
--- src/view/dashboard/Dashboard.js
+++ src/view/dashboard/Dashboard.js
@@ -26,7 +26,7 @@ const Dashboard = () => {
             <span className="hr-horizon"></span><img src={Doiintanon} alt="doiintanonLogo" width={"20%"} />
           </div>
         </Col>
-        <Col lg={10} className="d-flex justify-content-center">
+        <Col xs={24} sm={24} md={24} lg={10} className="d-flex justify-content-center">
           <Radio.Group onChange={(e) => setKey(e.target.value)} value={key} >
             <Radio.Button value="respon">ผู้ตอบแบบสอบถาม</Radio.Button>
             <Radio.Button value="economic">มูลค่าทางเศรษฐกิจ</Radio.Button>
