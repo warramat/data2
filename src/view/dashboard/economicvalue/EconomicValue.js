60afd3f57fa28a99410d63fb8176ffeeca7575b1
unknown <Palmseena41@gmail.com>
2020-11-06 16:32:42 +0700

    new5

--- src/view/dashboard/economicvalue/EconomicValue.js
+++ src/view/dashboard/economicvalue/EconomicValue.js
@@ -3,71 +3,64 @@ import { Row, Col, Card, Modal } from "antd";
 import RadarChart from "./Chart2/RadarChart";
 import StackedBar from "./Chart2/StackBar";
 import Spending from "./Spending";
-import { GET_ECONOMICVALUE, GET_ECONOMIC_TYPE } from "../../../service/api"
+import { GET_ECONOMICVALUE, GET_ECONOMIC_TYPE } from "../../../service/api";
 import ValueEco from "./value/ValueEco";
-import { float1Point } from "../../../tools/util"
-import { ChangeToK } from "../../../tools/util"
-
-
+import { float1Point } from "../../../tools/util";
+import { ChangeToK } from "../../../tools/util";
 
 const EconomicValue = ({ setLoading }) => {
-  const [economicEffect, setEconomicEffect] = useState({})
-  const [economicImpact, setEconomicImpact] = useState({})
-  const [economicIncreaseLabels, setEconomicIncreaseLabels] = useState([])
-  const [economicIncreaseData, setEconomicIncreaseData] = useState([])
-  const [questionnaireGroup, setQuestionnaireGroup] = useState([])
-  const [visible, setVisible] = useState(false)
-  const [textHeader, setTextHeader] = useState("")
-  const [economicModalValue, setEconomicModalValue] = useState({})
-
+  const [economicEffect, setEconomicEffect] = useState({});
+  const [economicImpact, setEconomicImpact] = useState({});
+  const [economicIncreaseLabels, setEconomicIncreaseLabels] = useState([]);
+  const [economicIncreaseData, setEconomicIncreaseData] = useState([]);
+  const [questionnaireGroup, setQuestionnaireGroup] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [textHeader, setTextHeader] = useState("");
+  const [economicModalValue, setEconomicModalValue] = useState({});
 
   useEffect(() => {
     GetEconomicValue();
   }, []);
 
   const GetEconomicValue = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const qrs = { evaluatortype: "all" }
-      const res = await GET_ECONOMICVALUE(qrs)
+      const qrs = { evaluatortype: "all" };
+      const res = await GET_ECONOMICVALUE(qrs);
       if (res.code === 200) {
-        setEconomicEffect(res.result[0].economicEffect)
-        setEconomicImpact(res.result[0].economicImpact)
-        setEconomicIncreaseLabels(res.result[0].economicIncreaseInValue.label)
-        setEconomicIncreaseData(res.result[0].economicIncreaseInValue.data)
-        setQuestionnaireGroup(res.result[0].questionnaireGroup)
-
+        setEconomicEffect(res.result[0].economicEffect);
+        setEconomicImpact(res.result[0].economicImpact);
+        setEconomicIncreaseLabels(res.result[0].economicIncreaseInValue.label);
+        setEconomicIncreaseData(res.result[0].economicIncreaseInValue.data);
+        setQuestionnaireGroup(res.result[0].questionnaireGroup);
       } else {
         alert("ERROR");
-
       }
     } catch (error) {
       console.log(error);
-      setLoading(false)
+      setLoading(false);
     }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   const handleModal = async (valueModal) => {
-    setLoading(true)
-    setTextHeader(valueModal.choiceTh)
+    setLoading(true);
+    setTextHeader(valueModal.choiceTh);
     try {
-      const qrs = { evaluatortype: valueModal.evaluatortype }
-      const res = await GET_ECONOMIC_TYPE(qrs)
+      const qrs = { evaluatortype: valueModal.evaluatortype };
+      const res = await GET_ECONOMIC_TYPE(qrs);
       if (res.code === 200) {
-        setEconomicModalValue(res.result[0].economicImpact)
-        setVisible(true)
+        setEconomicModalValue(res.result[0].economicImpact);
+        setVisible(true);
       } else {
         alert("ERROR");
       }
-
     } catch (error) {
       console.log(error);
-      setLoading(false)
+      setLoading(false);
     }
-    setLoading(false)
-
-  }
+    setLoading(false);
+  };
 
   return (
     <div>
@@ -77,38 +70,45 @@ const EconomicValue = ({ setLoading }) => {
         footer={false}
         width={1000}
       >
-        <StackedBar economicStackedBar={economicModalValue} title={textHeader} />
+        <StackedBar
+          economicStackedBar={economicModalValue}
+          title={textHeader}
+        />
       </Modal>
       <Row justify={"center"} gutter={[15, 15]}>
-        {
-          questionnaireGroup.map((item, index) => {
-            return (
-              <Col lg={4} key={index}>
-                <Card onClick={() => handleModal(item)} className="interactive">
-                  <ValueEco 
-                        toppic={item.choiceTh} 
-                        num={ChangeToK(item.count)} 
-                        persen={float1Point(item.percent)} 
-                        imageIndex={index} 
-                  />
-                </Card>
-              </Col>
-            )
-          })
-        }
+        {questionnaireGroup.map((item, index) => {
+          return (
+            <Col lg={4} sm={24} md={8} xs={24} key={index}>
+              <Card onClick={() => handleModal(item)} className="interactive">
+                <ValueEco
+                  toppic={item.choiceTh}
+                  num={ChangeToK(item.count)}
+                  persen={float1Point(item.percent)}
+                  imageIndex={index}
+                />
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
       <Row justify={"center"} gutter={[15, 15]}>
-        <Col lg={8}>
+        <Col lg={8} sm={24} md={24} xs={24}>
           <Card>
-            <StackedBar economicStackedBar={economicImpact} title={"ข้อมูลทางเศรษฐกิจ"} />
+            <StackedBar
+              economicStackedBar={economicImpact}
+              title={"ข้อมูลทางเศรษฐกิจ"}
+            />
           </Card>
         </Col>
-        <Col lg={8}>
+        <Col lg={8} sm={24} md={24} xs={24}>
           <Card>
-            <RadarChart economicLabels={economicIncreaseLabels} economicData={economicIncreaseData} />
+            <RadarChart
+              economicLabels={economicIncreaseLabels}
+              economicData={economicIncreaseData}
+            />
           </Card>
         </Col>
-        <Col lg={8}>
+        <Col lg={8} sm={24} md={24} xs={24}>
           <Card>
             <Spending economicEffect={economicEffect} />
           </Card>
@@ -117,6 +117,5 @@ const EconomicValue = ({ setLoading }) => {
     </div>
   );
 };
-
 
 export default EconomicValue;
