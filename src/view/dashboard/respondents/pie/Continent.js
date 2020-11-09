9c66b81a50d7214afc264273a34ad9d77ccd8202
warramat <warramat1999@gmail.com>
2020-11-09 16:50:12 +0700

    Update

--- src/view/dashboard/respondents/pie/Continent.js
+++ src/view/dashboard/respondents/pie/Continent.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Pie } from "react-chartjs-2"
 import { Row, Col } from "antd"
 import { ChangeToK } from '../../../../tools/util'
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 const Continent = ({ liveCountinent }) => {
   let sumDataLiveCountinent = []
